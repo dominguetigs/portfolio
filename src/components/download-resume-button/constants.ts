370fb80de1d02949aefbb1e2d12667490243ce45
Gustavo Domingueti <gustavo.s.domingueti@icloud.com>
2025-04-23 20:13:28 -0300

    feat: update button to download resume

--- src/components/download-resume-button/constants.ts
+++ src/components/download-resume-button/constants.ts
@@ -5,12 +5,12 @@ export const RESUME_OPTIONS = (t: TranslationFunction) => [
     label: t('resumeEnglish'),
     value: 'en',
     path: '/cv/resume-en.pdf',
-    filename: 'Gustavo_Domingueti_Resume_PT.pdf',
+    filename: 'Gustavo_Domingueti_Resume_EN.pdf',
   },
   {
     label: t('resumePortuguese'),
     value: 'pt',
     path: '/cv/resume-pt.pdf',
-    filename: 'Gustavo_Domingueti_Resume_EN.pdf',
+    filename: 'Gustavo_Domingueti_Resume_PT.pdf',
   },
 ];
